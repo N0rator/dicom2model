97ae79b7b84e5259b0cbd92a75ef5fca3b850fbc
Maksim Dovydenko <maksimdovydenko@protonmail.com>
2019-09-29 16:37:05 +0300

    front-1 initiate client app structure, update .gitignore

---
+++ client/src/main-page.component.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Card, Container} from "react-bootstrap";
+
+export default function MainPage() {
+    return (<>
+        <Container>
+            <Card>
+                <Card.Body>
+                    Hello?
+                </Card.Body>
+            </Card>
+        </Container>
+    </>);
+}
