6f3ebacbd1100c9a1f482b6791937c7d24b78783
Max-Dov <maksimdovydenko@protonmail.com>
2020-01-12 23:12:33 +0300

    front-1 add UI form mock-ups

--- client/src/main-page.component.tsx
+++ client/src/main-page.component.tsx
@@ -1,12 +1,38 @@
 import React from 'react';
-import {Card, Container} from "react-bootstrap";
+import {Button, Card, Container, Form} from "react-bootstrap";
+import FileInput from "./components/file-input.component";
 
 export default function MainPage() {
+    let files: FileList;
+
+    let handleSubmit = () => {
+
+    };
+
+    let onFileSelection = (selectedFiles: FileList) => {
+        files = selectedFiles;
+    };
+
     return (<>
-        <Container>
+        <Container className={'pt-10'}>
+            <Card className={'mb-10 footer'}>
+                <Card.Body>
+                    DICOM -> MODEL
+                </Card.Body>
+            </Card>
             <Card>
                 <Card.Body>
-                    Hello?
+                    <Form> {/* files form */}
+                        <Form.Group controlId="filesForm.ControlName">
+                            <Form.Control type="email" placeholder="Enter Your Name"/>
+                        </Form.Group>
+                        <Form.Group controlId="filesForm.ControlFiles">
+                            <FileInput onFilesSelection={onFileSelection}/>
+                        </Form.Group>
+                        <Button variant="success" type="button" onClick={handleSubmit}>
+                            Submit
+                        </Button>
+                    </Form>
                 </Card.Body>
             </Card>
         </Container>
